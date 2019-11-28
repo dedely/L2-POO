ffe30a2008d84d78676b71fbe91f7a7657fce9b9
Slydz <paul.bourin@hotmail.fr>
2019-11-28 15:37:12 +0100

    Update of method comparison (oklm)

--- Playground/src/infos/Fichier.java
+++ Playground/src/infos/Fichier.java
@@ -1,6 +1,5 @@
 package infos;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,8 +9,14 @@ import java.util.Iterator;
 
 public class Fichier {
 
+	public final static String SEPARATOR =";";
+
+	private String extension;
+	private String mime;
+
 	private FileReader fr;
-	private ArrayList<String> tab;
+	private ArrayList<String> tab = new ArrayList<String>();
+
 
 	public Fichier (String name) {
 		try {
@@ -20,24 +25,90 @@ public class Fichier {
 		catch (FileNotFoundException e) {
 			System.err.println(e.getMessage());
 		}
+		FileInfos testClass = new FileInfos();
+		this.extension = testClass.getExtensionUsingApacheCommonLib(name);
+		System.out.println(testClass.getExtensionUsingApacheCommonLib(name));
+		this.mime=testClass.getMimeUsingTika(name);
+		System.out.println(testClass.getMimeUsingTika(name));
 	}
+
+
 
 	public void read() 
 	{
 		String line="";
 		try {
 			BufferedReader reader = new BufferedReader(fr);
+			while((line=reader.readLine())!=null) 
+				System.out.println(line);
+			reader.close();
+		}
+		catch(IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+
+
+
+
+	public void comparaison() 
+	{
+		String line="";
+		String testExt="";
+		String testMime="";
+		String testSign="";
+		String fields[];
+
+
+		// STOCKAGE DU FICHIER CSV
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("signatures.csv"));
 			while ((line=reader.readLine()) != null) {
-				tab.add(line);
+				fields=line.split(SEPARATOR);
+				tab.add(fields[0]);
+				tab.add(fields[1]);
+				tab.add(fields[2]);
 			}
 			reader.close();
 		}
 		catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
+
+
+
+
+		// PARCOURS JUSQU'A TROUVER L'EXTENSION
+
+
 		Iterator<String> iter = tab.iterator();
-		while(iter.hasNext()) {
-			System.out.println(iter.next());
+		while (iter.hasNext()) {
+			testExt=iter.next();
+			if (testExt.equals(this.extension)) {
+				System.out.println(testExt + "  est l'extension de votre fichier\n");
+
+				System.out.println((testMime=iter.next()) + "  est le type mime de votre fichier\n");
+
+				System.out.println((testSign=iter.next()) + "  est la signature censee apparaitre dans votre fichier\n");
+			}
+
 		}
+		if (testMime.equals(""))
+			System.out.println("Votre extension n'existe pas\n");
+
+
+		// COMPARAISON DES TYPES MIMES
+
+		if (!testMime.equals(this.mime)) {
+			System.out.println("votre fichier n'a pas la bonne extension, car le type MIME ne correspond pas");
+		}
+		else {
+			System.out.println(" votre fichier semble etre un fichier " + this.extension);
+		}
+	
+
+
 	}
 }
