6425feb69aef6c9af337992d3ad9fe485fd488f2
JustAdel <adabbs2014@gmail.com>
2019-11-28 10:26:14 +0100

    updated testExtension

--- Playground/src/infos/TestExtension.java
+++ Playground/src/infos/TestExtension.java
@@ -3,7 +3,7 @@ package infos;
 public class TestExtension {
 
 	public static void main(String[] args) {
-		String test = "C:/test/nicetry.html";
+		String test = "test.txt";
 		Extension testClass = new Extension();
 		System.out.println(testClass.getExtensionUsingApacheCommonLib(test));
 		System.out.println(testClass.getMimeUsingTika(test));
